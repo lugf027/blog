0669a6431b05c3e73595aae34467bc0f5219a303
donaldlu <donaldlu@tencent.com>
2026-02-09 15:07:16 +0800

    init

---
+++ src/main/kotlin/io/github/lugf027/blog/BlogApplication.kt
@@ -0,0 +1,11 @@
+package io.github.lugf027.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogApplication>(*args)
+}
